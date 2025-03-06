d5dcb4e7780b1e9f7de7fd9306a88e8acd616b1a
Ridwan Ramadhan <erputramadhan@gmail.com>
2025-03-06 08:52:22 +0700

    nambah login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Card, Text, Button, YStack } from 'tamagui'
+import { TamaguiProvider, Card, Stack, Input, Button, Text, YStack, Image} from 'tamagui';
+
 
 const MyCard = () => {
   return (
@@ -19,8 +20,35 @@ const MyCard = () => {
 
 export default function App() {
   return (
-    <YStack f={1} jc="center" ai="center" bg="$background" p="$6">
-      <MyCard />
-    </YStack>
+    <Stack flex={1} justifyContent="center" alignItems="center" bg="" padding={20} minHeight="100vh" width="100vw">
+      <YStack 
+        width="100%" 
+        height="100vh" 
+        borderRadius={0} 
+        padding={25} 
+        bg="white" 
+        shadow="lg" 
+        alignItems="center" 
+        justifyContent="center"
+      >
+        <Image 
+          source={{ uri: '/logo.png' }} 
+          width={250} 
+          height={150}
+          aspectRatio={1/1}
+        />
+        <Text fontSize={26} fontWeight="bold" color="#343434" textAlign="center">
+          Welcome Back!
+        </Text>
+        <Text color="#343434" textAlign="center" marginBottom={15}>
+          Please login to continue
+        </Text>
+        <Input placeholder="Email" borderColor="$color6" padding={12} borderRadius={10} width="90%" maxWidth={400} />
+        <Input placeholder="Password" secureTextEntry borderColor="$color6" marginTop={12} padding={12} borderRadius={10} width="90%" maxWidth={400} />
+        <Button marginTop={20} bg="#273aff" pressStyle={{ bg: '$green10' }} width="90%" maxWidth={400} padding={12} borderRadius={10}>
+          <Text color="$color1" fontSize={16}>Sign In</Text>
+        </Button>
+      </YStack>
+    </Stack>
   )
 }
